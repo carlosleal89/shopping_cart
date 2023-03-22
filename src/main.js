f01cef8b5f8b141e0612c20bfff2a602402539ca
Carlos Leal <carlos.frleal@gmail.com>
2023-03-22 20:13:39 -0300

    requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
+
+const productsEl = document.querySelector('.products');
+const productsArray = await fetchProductsList('computador');
+productsArray.forEach((product) => {
+  const productEl = createProductElement(product);
+  productsEl.append(productEl);
+});
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
