8b3e5de559c973c110af896c03b4863c6efe0a06
Carlos Leal <carlos.frleal@gmail.com>
2023-03-22 20:34:03 -0300

    requisito 4

--- src/main.js
+++ src/main.js
@@ -4,7 +4,22 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 
 const productsEl = document.querySelector('.products');
+
+function insertLoadingText() {
+  const loadingTextEl = document.createElement('p');
+  loadingTextEl.innerHTML = 'carregando...';
+  loadingTextEl.className = 'loading';
+  productsEl.append(loadingTextEl);
+}
+
+function removeLoadingText() {
+  productsEl.innerHTML = '';
+}
+
+insertLoadingText();
+
 const productsArray = await fetchProductsList('computador');
+removeLoadingText();
 productsArray.forEach((product) => {
   const productEl = createProductElement(product);
   productsEl.append(productEl);
