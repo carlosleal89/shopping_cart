bd6a468c20055e9db139593fce96db9522eb10c3
Carlos Leal <carlos.frleal@gmail.com>
2023-03-22 15:05:53 -0300

    requisito 1

--- src/main.js
+++ src/main.js
@@ -2,4 +2,3 @@ import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
- 
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,16 +5,26 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
   it('fetch é chamado ao executar fetchProductsList', () => {
-
+    expect(fetchProductsList('computador')).toHaveBeenCalled;
   });
 
   it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetchProductsList('computador')).toHaveBeenCalledWith(endpoint);
   });
+
+  it('verifica se o retorno da função fetchProductsList é o esperado', async () => {
+    const expectedReturn = await fetchProductsList('computador');
+    expect(expectedReturn).toEqual(computadorSearch);
+  })
+
+  it('verifica se a função retorna um erro quando não é passado um argumento de busca', () => {
+    expect(() => fetchProductsList()).toThrowError(error);
+  })
 
   // it('...', () => {
   // });
