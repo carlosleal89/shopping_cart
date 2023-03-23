e24c3d1fd908ec59fa943e63c6768e3cd04318fe
Carlos Leal <carlos.frleal@gmail.com>
2023-03-22 22:17:41 -0300

    requisito 6 done e 7 ongoing

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,16 +1,19 @@
-export const fetchProduct = () => {
-  // seu código aqui
+export const fetchProduct = (id) => {
+  if (!id) {
+    throw new Error('ID não informado');
+  }
+  const endpoint = 'https://api.mercadolibre.com/items/';
+  return fetch(`${endpoint}${id}`)
+    .then((response) => response.json()).then((data) => data);
 };
 
 export const fetchProductsList = (product) => {
   if (!product) {
     throw new Error('Termo de busca não informado');
   }
-  try {
-    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=';
-    return fetch(`${endpoint}${product}`)
-      .then((response) => response.json()).then((data) => data.results);
-  } catch (error) {
-    return error.message;
-  }
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+  return fetch(`${endpoint}${product}`)
+    .then((response) => response.json()).then((data) => data.results);
 };
+
+console.log(fetchProduct());
--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,22 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('testa se fetchFunctions é uma função', () => {
+    expect(typeof fetchProduct).toBe('function');
+  });
+  it('fetch é chamado ao executar fetchProduct', () => {
+    expect(fetchProduct('MLB1405519561')).toHaveBeenCalled;
+  });
+  it('fetch é chamado com o endpoint correto ao executar fetchProduct', async () => {
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1405519561';
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+  it('verifica se o retorno da função fetchProduct é o esperado', async () => {
+    const expectedReturn = await fetchProduct('MLB1405519561');
+    expect(expectedReturn).toEqual(product);
+  });
+  it('verifica se a função retorna um erro quando não é passado um argumento de busca', () => {
+    expect(() => fetchProduct()).toThrow(new Error ('ID não informado'));
   });
 });
